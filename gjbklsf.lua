46442ccd857e231967250ca580b2d4f3a2f6549e
vbbi33jkllljcvvbjkqw89fjjkjhkjzsdaih <jin.chin.min@gmail.com>
2026-04-07 16:30:14 +0100

    Update gjbklsf.lua

--- gjbklsf.lua
+++ gjbklsf.lua
@@ -1,1 +1,1 @@
-game:GetService("Players").LocalPlayer:Kick("updating wait for DIH fix")
+game:GetService("Players").LocalPlayer:Kick("you have been perma banned for using fenti SEND THIS IN FENTI DISCORD (joke)")
